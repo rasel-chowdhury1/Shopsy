616fe29361b9995fde70479fe58433200ff635a7
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-02-01 21:06:30 +0600

    added body section in topProducts component from Products component

--- src/Components/TopProducts/TopProducts.jsx
+++ src/Components/TopProducts/TopProducts.jsx
@@ -10,7 +10,34 @@ const TopProducts = () => {
                     <h1 data-aos='fade-up' className='text-3xl font-bold'>Best Products</h1>
                     <p data-aos='fade-up'  className='text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut expedita esse odio blanditiis eveniet numquam.</p>
                 </div>
-                {/** Body Section */}
+                {/** body section */}
+                <div>
+                    <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-3'>
+                        {/** card section */}
+                        {
+                            ProductsData.map(data => (
+                                <div 
+                                 data-aos='fade-up'
+                                 data-aos-delay={data.aosDelay}
+                                 key={data.id} className='space-y-3'>
+                                    <img src={data.img} alt="" 
+                                    className='h-[220px] w-[150px] rounded-md object-cover'/>
+                                    <div>
+                                        <h3 className='font-semibold'>{data.title}</h3>
+                                        <p className="text-sm text-gray-600">
+                                            {data.color}
+                                        </p>
+                                        <div className='flex items-center gap-1'>
+                                            <FaStar className='text-yellow-500' />
+                                            <span>{data.rating}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+
+                    </div>
+                </div>
             </div>
         </div>
     );
